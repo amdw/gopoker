9dc925e820f9ca941583043cbe68bd433fbcca3b
Andrew Medworth <andrew@medworth.org.uk>
2013-05-26 21:13:41 +0800

    Fixed copying notice

--- src/poker/poker_test.go
+++ src/poker/poker_test.go
@@ -15,7 +15,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with Gopoker.  If not, see <http://www.gnu.org/licenses/>.
 */
 package poker
 
