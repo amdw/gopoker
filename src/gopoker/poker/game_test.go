4fccaac3062d6882fc96e3915179f71ce87446ee
Andrew Medworth <github@medworth.org.uk>
2015-01-17 10:56:54 +0800

    Improve fixed shuffle test

--- src/gopoker/poker/game_test.go
+++ src/gopoker/poker/game_test.go
@@ -106,35 +106,48 @@ func TestHoldem(t *testing.T) {
 
 func TestFixedShuffle(t *testing.T) {
 	pack := NewPack()
-	myCards := h("KS", "AC")
-	tableCards := h("10D", "2C", "AS")
 	pack.randGen = rand.New(rand.NewSource(1234)) // Deterministic for repeatable tests
 
-	for i := 0; i < 100; i++ {
-		pack.Shuffle()
-		pack.shuffleFixing(tableCards, myCards)
-		for j, c := range h("10D", "2C", "AS") {
-			if pack.Cards[j] != c {
-				t.Fatalf("Expected %v at Cards[%v], found %v", c, j, pack.Cards[j])
-			}
-		}
-		for j, c := range h("KS", "AC") {
-			if pack.Cards[j+5] != c {
-				t.Fatalf("Expected %v at Cards[%v], found %v", c, j+5, pack.Cards[j+5])
-			}
-		}
-		permCheck := make([][]int, 4)
-		for i := 0; i < 4; i++ {
-			permCheck[i] = make([]int, 13)
-		}
-		for _, c := range pack.Cards {
-			permCheck[c.Suit][c.Rank]++
-		}
-		for s := range permCheck {
-			for r, count := range permCheck[s] {
-				if count != 1 {
-					t.Fatalf("Expected exactly one %v%v in pack after shuffle, found %v", Rank(r).String(), Suit(s).String(), count)
+	myCards := h("KS", "AC")
+	tableCards := h("10D", "2C", "AS", "4D", "6H")
+	pack.Shuffle()
+	pack.shuffleFixing(tableCards, myCards)
+	tCards, pCards, _ := pack.PlayHoldem(2)
+	if !cardsEqual(tableCards, tCards) {
+		t.Errorf("Expected table cards %q, found %q", tableCards, tCards)
+	}
+	if !cardsEqual(myCards, pCards[0]) {
+		t.Errorf("Expected player 1 cards %q, found %q", myCards, pCards[0])
+	}
+	containsAny := func(cards, testCards []Card) bool {
+		for _, c := range cards {
+			for _, tc := range testCards {
+				if c == tc {
+					return true
 				}
+			}
+		}
+		return false
+	}
+	for i := 1; i < len(pCards); i++ {
+		if containsAny(pCards[i], myCards) {
+			t.Errorf("Player %v's cards %q should not contain any of player 1's cards %q.", i+1, pCards[i], myCards)
+		}
+		if containsAny(pCards[i], tableCards) {
+			t.Errorf("Player %v's cards %q should not contain any table cards %q", i+1, pCards[i], tableCards)
+		}
+	}
+	permCheck := make([][]int, 4)
+	for i := 0; i < 4; i++ {
+		permCheck[i] = make([]int, 13)
+	}
+	for _, c := range pack.Cards {
+		permCheck[c.Suit][c.Rank]++
+	}
+	for s := range permCheck {
+		for r, count := range permCheck[s] {
+			if count != 1 {
+				t.Fatalf("Expected exactly one %v%v in pack after shuffle, found %v", Rank(r).String(), Suit(s).String(), count)
 			}
 		}
 	}
