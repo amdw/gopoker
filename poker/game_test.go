6aacd1683d9a590a7737d87749f82ea5d632d4bc
Andrew Medworth <github@medworth.org.uk>
2015-07-04 17:37:24 +0800

    Add missing headers

--- poker/game_test.go
+++ poker/game_test.go
@@ -1,3 +1,22 @@
+/*
+Copyright 2013 Andrew Medworth
+
+This file is part of Gopoker, a set of miscellaneous poker-related functions
+written in the Go programming language (http://golang.org).
+
+Gopoker is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Gopoker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Gopoker.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package poker
 
 import (
