42c3389aba62a0903910234869445b05d8005102
Andrew Medworth <github@medworth.org.uk>
2016-01-30 19:14:35 +0800

    Make hl function take varargs and use in game_test.go (closes #12)

--- poker/hands_test.go
+++ poker/hands_test.go
@@ -52,15 +52,15 @@ type LevelTest struct {
 	isLess    bool
 }
 
-func hl(class HandClass, tiebreaks []Rank) HandLevel {
-	return HandLevel{class, tiebreaks}
+func hl(class HandClass, tieBreaks ...Rank) HandLevel {
+	return HandLevel{class, tieBreaks}
 }
 
 var levelTests = []LevelTest{
-	{hl(StraightFlush, []Rank{Ace}), hl(StraightFlush, []Rank{Ace}), false, false},
-	{hl(StraightFlush, []Rank{Ace}), hl(StraightFlush, []Rank{King}), true, false},
-	{hl(FourOfAKind, []Rank{Nine, Ten}), hl(StraightFlush, []Rank{Two}), false, true},
-	{MinLevel(), hl(HighCard, []Rank{Two, Two, Two, Two, Three}), false, true},
+	{hl(StraightFlush, Ace), hl(StraightFlush, Ace), false, false},
+	{hl(StraightFlush, Ace), hl(StraightFlush, King), true, false},
+	{hl(FourOfAKind, Nine, Ten), hl(StraightFlush, Two), false, true},
+	{MinLevel(), hl(HighCard, Two, Two, Two, Two, Three), false, true},
 }
 
 func TestLevels(t *testing.T) {
@@ -94,23 +94,23 @@ func h(cards ...string) []Card {
 // Actually there are no mandatory cards in Hold'em, but these tests are still valid.
 // This code is indirectly tested in game_test.go, which contains more realistic Hold'em scenarios.
 var classTests = []ClassTest{
-	ClassTest{h("AS", "KS", "QS", "JS", "10S"), h(), HandLevel{StraightFlush, []Rank{Ace}}, h("AS", "KS", "QS", "JS", "10S")},
-	ClassTest{h("9D", "10S", "9S", "9H", "9C"), h(), HandLevel{FourOfAKind, []Rank{Nine, Ten}}, h("9D", "10S", "9S", "9H", "9C")},
-	ClassTest{h("10S", "JS"), h("2H", "QS", "6D", "KS", "AS"), HandLevel{StraightFlush, []Rank{Ace}}, h("10S", "JS", "QS", "KS", "AS")},
-	ClassTest{h("10S", "JS"), h("10C", "QD", "10D", "10H", "2S"), HandLevel{FourOfAKind, []Rank{Ten, Jack}}, h("10S", "10C", "10D", "10H", "JS")},
-	ClassTest{h("10S", "10C"), h("10D", "10H", "JD", "QD", "KD", "AD"), HandLevel{FourOfAKind, []Rank{Ten, Ace}}, h("10S", "10C", "10D", "10H", "AD")},
-	ClassTest{h("2S", "2H"), h("3D", "3H", "3C", "QD", "KS"), HandLevel{FullHouse, []Rank{Three, Two}}, h("2S", "2H", "3H", "3C", "3D")},
-	ClassTest{h("2S", "3S"), h("4H", "4D", "4C", "4S", "2D", "2H", "3C"), HandLevel{FullHouse, []Rank{Two, Three}}, h("2S", "2D", "2H", "3S", "3C")},
-	ClassTest{h("6H", "8H"), h("9H", "10H", "2H", "3S", "7C"), HandLevel{Flush, []Rank{Ten, Nine, Eight, Six, Two}}, h("10H", "9H", "8H", "6H", "2H")},
-	ClassTest{h("6S", "8H"), h("9H", "10H", "JH", "QH", "7H"), HandLevel{Straight, []Rank{Ten}}, h("10H", "9H", "8H", "7H", "6S")},
-	ClassTest{h("AS", "3H"), h("2C", "4C", "5D", "KS", "JC"), HandLevel{Straight, []Rank{Five}}, h("AS", "2C", "3H", "4C", "5D")},
-	ClassTest{h("6S", "6D"), h("6C", "KH", "JC", "7H", "2S"), HandLevel{ThreeOfAKind, []Rank{Six, King, Jack}}, h("6S", "6D", "6C", "KH", "JC")},
-	ClassTest{h("6S", "2S"), h("6C", "KH", "JC", "7H", "6D"), HandLevel{ThreeOfAKind, []Rank{Six, King, Two}}, h("6S", "6D", "6C", "KH", "2S")},
-	ClassTest{h("6S", "4D"), h("6D", "QS", "4S", "AH", "3C"), HandLevel{TwoPair, []Rank{Six, Four, Ace}}, h("6S", "6D", "4D", "4S", "AH")},
-	ClassTest{h("6S", "6D"), h("4D", "QS", "4S", "AH", "3C"), HandLevel{TwoPair, []Rank{Six, Four, Ace}}, h("6S", "6D", "4D", "4S", "AH")},
-	ClassTest{h("KS", "8C"), h("10H", "10C", "9H", "7H", "6S"), HandLevel{OnePair, []Rank{Ten, King, Nine, Eight}}, h("KS", "8C", "10H", "10C", "9H")},
-	ClassTest{h("KS", "10H"), h("8C", "10C", "9H", "7H", "6S"), HandLevel{OnePair, []Rank{Ten, King, Nine, Eight}}, h("KS", "8C", "10H", "10C", "9H")},
-	ClassTest{h("KS", "10H"), h("8C", "5C", "9H", "7H", "6S"), HandLevel{HighCard, []Rank{King, Ten, Nine, Eight, Seven}}, h("KS", "10H", "9H", "8C", "7H")},
+	ClassTest{h("AS", "KS", "QS", "JS", "10S"), h(), hl(StraightFlush, Ace), h("AS", "KS", "QS", "JS", "10S")},
+	ClassTest{h("9D", "10S", "9S", "9H", "9C"), h(), hl(FourOfAKind, Nine, Ten), h("9D", "10S", "9S", "9H", "9C")},
+	ClassTest{h("10S", "JS"), h("2H", "QS", "6D", "KS", "AS"), hl(StraightFlush, Ace), h("10S", "JS", "QS", "KS", "AS")},
+	ClassTest{h("10S", "JS"), h("10C", "QD", "10D", "10H", "2S"), hl(FourOfAKind, Ten, Jack), h("10S", "10C", "10D", "10H", "JS")},
+	ClassTest{h("10S", "10C"), h("10D", "10H", "JD", "QD", "KD", "AD"), hl(FourOfAKind, Ten, Ace), h("10S", "10C", "10D", "10H", "AD")},
+	ClassTest{h("2S", "2H"), h("3D", "3H", "3C", "QD", "KS"), hl(FullHouse, Three, Two), h("2S", "2H", "3H", "3C", "3D")},
+	ClassTest{h("2S", "3S"), h("4H", "4D", "4C", "4S", "2D", "2H", "3C"), hl(FullHouse, Two, Three), h("2S", "2D", "2H", "3S", "3C")},
+	ClassTest{h("6H", "8H"), h("9H", "10H", "2H", "3S", "7C"), hl(Flush, Ten, Nine, Eight, Six, Two), h("10H", "9H", "8H", "6H", "2H")},
+	ClassTest{h("6S", "8H"), h("9H", "10H", "JH", "QH", "7H"), hl(Straight, Ten), h("10H", "9H", "8H", "7H", "6S")},
+	ClassTest{h("AS", "3H"), h("2C", "4C", "5D", "KS", "JC"), hl(Straight, Five), h("AS", "2C", "3H", "4C", "5D")},
+	ClassTest{h("6S", "6D"), h("6C", "KH", "JC", "7H", "2S"), hl(ThreeOfAKind, Six, King, Jack), h("6S", "6D", "6C", "KH", "JC")},
+	ClassTest{h("6S", "2S"), h("6C", "KH", "JC", "7H", "6D"), hl(ThreeOfAKind, Six, King, Two), h("6S", "6D", "6C", "KH", "2S")},
+	ClassTest{h("6S", "4D"), h("6D", "QS", "4S", "AH", "3C"), hl(TwoPair, Six, Four, Ace), h("6S", "6D", "4D", "4S", "AH")},
+	ClassTest{h("6S", "6D"), h("4D", "QS", "4S", "AH", "3C"), hl(TwoPair, Six, Four, Ace), h("6S", "6D", "4D", "4S", "AH")},
+	ClassTest{h("KS", "8C"), h("10H", "10C", "9H", "7H", "6S"), hl(OnePair, Ten, King, Nine, Eight), h("KS", "8C", "10H", "10C", "9H")},
+	ClassTest{h("KS", "10H"), h("8C", "10C", "9H", "7H", "6S"), hl(OnePair, Ten, King, Nine, Eight), h("KS", "8C", "10H", "10C", "9H")},
+	ClassTest{h("KS", "10H"), h("8C", "5C", "9H", "7H", "6S"), hl(HighCard, King, Ten, Nine, Eight, Seven), h("KS", "10H", "9H", "8C", "7H")},
 }
 
 func levelsEqual(l1, l2 HandLevel) bool {
